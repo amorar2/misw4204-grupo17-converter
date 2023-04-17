e8e8a86616b1c5cd039e1a0365c2b1f75e131134
Juan Henao <jhenaoz93@gmail.com>
2023-04-16 22:26:10 -0500

    performance test

--- README.md
+++ README.md
@@ -5,11 +5,11 @@
 ## Running performance tests
 
 1. Install k6 bin, https://k6.io/docs/get-started/installation/
-2. set up local env, python, pg, redis.
+2. set up local env, python, pg, redis. docker-compose up
 3. run the tests locally
 
 ```
-  k6 run ./performance/performance-test.js
+  k6 run --out influxdb=http://localhost:8086/k6 ./performance/performance-test.js
 ```
 
 #### Note: k6 test are able to run on clod or in a distributed environment, for this scenario we are just using local execution.
--- samples/performance-test.js
+++ samples/performance-test.js
@@ -5,23 +5,17 @@ import { check, sleep } from 'k6';
 export const options = {
     stages: [
       { duration: '10s', target: 5 },
-      // { duration: '20s', target: 15 },
-      // { duration: '20s', target: 25 },
-      // { duration: '20s', target: 35 },
-      // { duration: '20s', target: 45 },
-      // { duration: '20s', target: 20 },
-      // { duration: '20s', target: 5 },
-      // { duration: '20s', target: 0 },
+      { duration: '20s', target: 15 },
+      { duration: '20s', target: 25 },
+      { duration: '20s', target: 35 },
+      { duration: '20s', target: 45 },
+      { duration: '20s', target: 20 },
+      { duration: '20s', target: 5 },
+      { duration: '20s', target: 0 },
     ],
 };
 
 export default function () {
-  // 3. VU code, this code is executed by each virtual user in k6
-  // const res = http.get('https://httpbin.test.k6.io/');
-  // check(res, { 'status was 200': (r) => r.status == 203 });
-  // sleep(1);
-
-
   let res = http.post('http://127.0.0.1:5001/login', JSON.stringify({username: 'admin', password: 'admin'}), {
     headers: { 'Content-Type': 'application/json' },
   } );
